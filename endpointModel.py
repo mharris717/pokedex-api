acc7c8760495df61e92d1bd8a7a3871a2a8cb882
Mike Harris <mharris717@gmail.com>
2024-02-28 12:04:54 -0500

    add fetchMany and pagination

--- endpointModel.py
+++ endpointModel.py
@@ -1,17 +1,52 @@
-from typing import ClassVar
+from dataclasses import dataclass
+from typing import Any, ClassVar
 
 import requests
 from pydantic import BaseModel
+
+from apiResource import namedApiResource
 
 
 class EndpointModel(BaseModel):
     url: ClassVar[str]
 
     @classmethod
-    def fetch(cls, id: int):
+    def fetchOne(cls, id: int):
         response = requests.get(f"https://pokeapi.co/api/v2/{cls.url}/{id}")
         if response.status_code == 200:
             data = response.json()
             return cls(**data)
+        else:
+            return None
+
+    @classmethod
+    def fetchMany(cls, **kwargs):
+        url = f"https://pokeapi.co/api/v2/{cls.url}"
+        if kwargs:
+            url += "?" + "&".join(f"{k}={v}" for k, v in kwargs.items())
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return Page(cls, data)
+        else:
+            return None
+
+
+@dataclass
+class Page:
+    cls: Any
+    data: Any
+
+    @property
+    def results(self):
+        resource = namedApiResource(self.cls)
+        return [resource(**result) for result in self.data["results"]]
+
+    def fetchNext(self):
+        response = requests.get(self.data["next"])
+        if response.status_code == 200:
+            data = response.json()
+            return Page(self.cls, data)
         else:
             return None
--- tests.py
+++ tests.py
@@ -1,5 +1,28 @@
 from main import Pokemon
 
 
+def assertBulbasaur(poke):
+    assert poke.abilities[0].ability.resolve().name == "overgrow"
+    assert poke.name == "bulbasaur"
+
+
 def testSmoke():
-    assert Pokemon.fetch(1).abilities[0].ability.resolve().name == "overgrow"
+    poke = Pokemon.fetchOne(1)
+    assertBulbasaur(poke)
+
+
+def testFetchList():
+    pokes = Pokemon.fetchMany()
+    assert len(pokes.results) == 20
+    assert pokes.results[0].name == "bulbasaur"
+    assertBulbasaur(pokes.results[0].resolve())
+    nextPokes = pokes.fetchNext()
+    assert len(nextPokes.results) == 20
+    assert nextPokes.results[0].name == "spearow"
+    assert nextPokes.results[0].resolve().name == "spearow"
+
+
+def testFetchListOps():
+    pokes = Pokemon.fetchMany(limit=5)
+    assert len(pokes.results) == 5
+    assert pokes.results[0].name == "bulbasaur"
